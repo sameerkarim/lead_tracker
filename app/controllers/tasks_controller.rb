8c39efb09eba22d8ddc28de561a0ae300759fb3c
sameerkarim <sameerkarim1@gmail.com>
2012-03-03 19:03:03 +0800

    Altered the contact form to better fit tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,17 @@ class TasksController < ApplicationController
     @task = @contact.tasks.create(params[:task])
     redirect_to contact_path(@contact)
   end
+  
+  def edit
+    #...
+  end
+
+
+  def destroy
+    @contact = Contact.find(params[:contact_id])
+    @task = @contact.tasks.find(params[:id])
+    @task.destroy
+    redirect_to contact_path(@contact)
+  end
 end
+
