1d046bcf4301fe00b0f30abf55a8b7e3662d8415
sameerkarim <sameerkarim1@gmail.com>
2012-03-03 21:20:10 +0800

    Commit before adding user signups

--- app/models/contact.rb
+++
@@ -1,5 +0,0 @@
-class Contact < ActiveRecord::Base
-  validates :name, presence: :true
-  
-  has_many :tasks
-end
--- app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ActiveRecord::Base
-  belongs_to :contact
-  
-  validates :title, presence: :true
-end
