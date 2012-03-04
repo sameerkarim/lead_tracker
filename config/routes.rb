ef66ddcf3d1f45c75496e18d3b97c94528730160
sameerkarim <sameerkarim1@gmail.com>
2012-03-04 14:51:23 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 LeadTracker::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   match '/signup', to: 'users#new'
-
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
   get "home/index"
   
   root :to => 'home#index'
